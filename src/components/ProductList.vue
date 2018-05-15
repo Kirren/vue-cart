856e84bf81e63f1ef271a0764915d782a0c1573c
Helen <kirren@live.com>
2018-05-15 12:15:24 +0300

    Change initial HelloWorld file to ProductList

---
+++ src/components/ProductList.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  div
+    h1 Product List
+    ul
+      li(v-for="product in products") {{product.title}} - {{product.price}}
+</template>
+
+<script>
+  import shop from '@/api/shop'
+  export default {
+    name: 'ProductList',
+    data () {
+      return {
+        products: []
+      }
+    },
+    created (){
+      shop.getProducts(products => {
+        this.products = products
+      })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
