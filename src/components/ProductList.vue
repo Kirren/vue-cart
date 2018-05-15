e808df587081a880cdf8752279c710c25b38c6eb
Helen <kirren@live.com>
2018-05-15 14:18:08 +0300

    Move store to be accessible from all components

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -7,8 +7,6 @@
 </template>
 
 <script>
-  import store from '@/store/index'
-
   export default {
     name: 'ProductList',
     data(){
@@ -18,12 +16,12 @@
     },
     computed: {
       products(){
-        return store.getters.availableProducts
+        return this.$store.getters.availableProducts
       }
     },
     created(){
       this.loading = true
-      store.dispatch('fetchProducts')
+      this.$store.dispatch('fetchProducts')
         .then(() => this.loading = false)
     }
   }
