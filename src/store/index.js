908b7e4cdc50461a58120bd01ad518aeb152da1e
Helen <kirren@live.com>
2018-05-15 14:15:18 +0300

    Add timeout and promise to fetchProducts

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import shop from '@/api/shop'
 
 Vue.use(Vuex)
 
@@ -16,7 +17,21 @@ export default new Vuex.Store({
   },
   actions: {
     // Make the call to the mock api to get the products array
-    fetchProducts(){}
+    fetchProducts({commit}){
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+          commit('setProducts', products)
+          resolve()
+        })
+      })
+    }/*,
+    addToCart(context, product){
+      if (product.inventory > 0) {
+        context.commit('pushProductToCart', product)
+      } else {
+
+      }
+    }*/
   },
   mutations: {
     // Update products state in the store
