72c404668fdbe42f19d5d48626b9f8d532208a0f
Helen <kirren@live.com>
2018-05-16 11:19:54 +0300

    Add vuex helpers {mapState, mapGetters, mapActions}

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+  import { mapState, mapGetters, mapActions } from 'vuex'
+
   export default {
     name: 'ProductList',
     data () {
@@ -18,21 +20,22 @@
       }
     },
     computed: {
-      products () {
-        return this.$store.state.products
-      },
-      productIsInStock () {
-        return this.$store.getters.productIsInStock
-      }
+      ...mapState({
+        products: state => state.products
+      }),
+      ...mapGetters({
+        productIsInStock: 'productIsInStock'
+      })
     },
     methods: {
-      addProductToCart (product) {
-        this.$store.dispatch('addProductToCart', product)
-      }
+      ...mapActions({
+        fetchProducts: 'fetchProducts',
+        addProductToCart: 'addProductToCart'
+      })
     },
     created () {
       this.loading = true
-      this.$store.dispatch('fetchProducts')
+      this.fetchProducts()
         .then(() => this.loading = false)
     }
   }
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -5,20 +5,26 @@
     ul
       li(v-for="product in products")
         | {{product.title}} - {{product.price | currency}} - {{product.quantity}}
-    button(@click="$store.dispatch('checkout')") Checkout
-    p(v-if="$store.state.checkoutStatus") {{$store.state.checkoutStatus}}
+    button(@click="checkout") Checkout
+    p(v-if="checkoutStatus") {{checkoutStatus}}
 </template>
 
 <script>
+  import { mapState, mapGetters, mapActions } from 'vuex'
+
   export default {
     name: 'ShoppingCart',
     computed: {
-      products () {
-        return this.$store.getters.cartProducts
-      },
-      total () {
-        return this.$store.getters.cartTotal
-      }
+      ...mapGetters({
+        products: 'cartProducts',
+        total: 'cartTotal'
+      }),
+      ...mapState({
+        checkoutStatus: 'checkoutStatus'
+      })
+    },
+    methods: {
+      ...mapActions(['checkout'])
     }
   }
 </script>
