e182459f0fcef53377e0bfd7ed92f7088c4ac2b3
Helen <kirren@live.com>
2018-05-18 12:05:00 +0300

    Remove comments

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -7,14 +7,12 @@ Vue.use(VueResource)
 export default {
   namespaced: true,
   state: {
-    // All items in catalogue
     items: []
   },
   getters: {
     availableProducts (state, getters) {
       return state.items.filter(product => product.quantity > 0)
     },
-    // Check if the product is left in stock
     productIsInStock () {
       return (product) => {
         return product.quantity > 0
@@ -22,21 +20,17 @@ export default {
     }
   },
   mutations: {
-    // Add items from api request to state
     setProducts (state, products) {
       state.items = products
     },
-    // Decrement item quantity on removal from cart
     incrementProductQuantity (state, product) {
       product.quantity++
     },
-    // Decrement item quantity on adding to cart
     decrementProductQuantity (state, product) {
       product.quantity--
     }
   },
   actions: {
-    // Commit api call to get products
     fetchProducts({commit}) {
       Vue.http.get(config.dev.apiHost + config.dev.apiHostPrefix + '/items')
         .then(response => {
